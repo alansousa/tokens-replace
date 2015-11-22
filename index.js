0a99bd247a9dbf98b5507780c4dbefac42a169bb
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-22 22:37:40 +0200

    Single replacement and multiple replacements from array

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ module.exports = function(source) {
   this.cacheable();
 
   var query = utils.parseQuery(this.query);
-  for (var i = 0; i < query.replace.length; i++) {
-    var option = query.replace[i];
+  if (typeof(query.replace) === 'object') {
+    for (var i = 0; i < query.replace.length; i++) {
+      var option = query.replace[i];
 
-    source = source.split(option.from).join(option.to)
+      source = source.split(option.from).join(option.to);
+    }
   }
+  else source = source.split(query.search).join(query.replace);
 
   return source;
 };
